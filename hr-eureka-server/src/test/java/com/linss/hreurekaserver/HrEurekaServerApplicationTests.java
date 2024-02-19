0d7ca234f446b87cb44078daaad8969712174593
marcelolmedeiros <back.dev.lins@gmail.com>
2024-02-19 12:00:42 -0300

    Configuring Eureka server

---
+++ hr-eureka-server/src/test/java/com/linss/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linss.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
